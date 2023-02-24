dbb770c7e87a4820e77bd57740a6bfb7aa33647a
Asuka <yanghang8612@163.com>
2023-02-24 12:11:13 +0800

    func(contract): move token contract and add interface

--- contracts/token/Token.sol
+++ contracts/token/Token.sol
@@ -15,9 +15,9 @@
 
 pragma solidity ^0.4.18;
 
-contract WETH9 {
-    string public name     = "Wrapped Ether";
-    string public symbol   = "WETH";
+contract WTOKEN9 {
+    string public name     = "Wrapped TBD";
+    string public symbol   = "WTBD";
     uint8  public decimals = 18;
 
     event  Approval(address indexed src, address indexed guy, uint wad);
